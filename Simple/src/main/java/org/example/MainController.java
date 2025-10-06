2753ae2c7a090e4206a33fc36c3a53377374866a
bjgo78 <bjgo78@gmail.com>
2025-10-06 09:25:42 +0200

    JDBC added
    Fixed MainController error
    Added JDBC

--- Simple/src/main/java/org/example/MainController.java
+++ Simple/src/main/java/org/example/MainController.java
@@ -9,7 +9,7 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainController {
-    SQLTest sqlTest;
+    SQLTest sqlTest = new SQLTest();
 
     @FXML
     private void initialize() {
