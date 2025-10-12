710bcef9f2703e16eda22cae2fd326585ba665df
lazarboti <161580615+lazarboti@users.noreply.github.com>
2025-10-12 12:48:28 +0200

    Dolgozo hozzáadása
    pop up window a dolgozo hozzá adásához

--- Simple/src/main/java/org/example/MainController.java
+++ Simple/src/main/java/org/example/MainController.java
@@ -7,6 +7,9 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 public class MainController {
     SQLQuery sqlQuery = new SQLQuery();
@@ -107,9 +110,48 @@ public class MainController {
         System.out.println(sqlQuery.searchEmployee(idField.getText(), nameField.getText(), jobTitleField.getText()));
     }
     @FXML
-    void employeeAddButton() {
-        sqlQuery.addEmployee("Teszt Pista", "HR");
-        sqlQuery.addPC("Dell", "Vostro 3070", "Teszt Pista");
+    void employeeAddButton(ActionEvent event) {
+        Stage popupStage = new Stage();
+        popupStage.setTitle("Dolgozó hozzáadása");
+
+        Label nameLabel = new Label("Név:");
+        TextField nameField = new TextField();
+
+        Label jobLabel = new Label("Beosztása:");
+        TextField jobField = new TextField();
+
+        Button addButton = new Button("Hozzáad");
+        addButton.setOnAction(e -> {
+            String name = nameField.getText();
+            String job = jobField.getText();
+
+            if (name.isEmpty() || job.isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setHeaderText(null);
+                alert.setContentText("Töltse ki a mezőket");
+                alert.showAndWait();
+            } else {
+                System.out.println(name +" "+ job);
+                //sqlQuery.addEmployee("Teszt Pista", "HR");
+                //sqlQuery.addPC("Dell", "Vostro 3070", "Teszt Pista");
+                popupStage.close();
+            }
+        });
+
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(15));
+        layout.getChildren().addAll(nameLabel, nameField, jobLabel, jobField, addButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 300, 220);
+
+        scene.getStylesheets().add(getClass().getResource("login.css").toExternalForm());
+
+        popupStage.setScene(scene);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.showAndWait();
+
     }
 
 
